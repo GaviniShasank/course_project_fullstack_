a7f96e99960fcaa5faa63295db1761b22738d50c
Gavini Shasank <130299325+GaviniShasank@users.noreply.github.com>
2025-08-26 23:39:19 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/google-auth", 
+  baseURL: "https://course-project-backend-liox.onrender.com/google-auth", 
   withCredentials: true,
 });
+
