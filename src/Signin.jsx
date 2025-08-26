b3ef609042c1809d4976db274ce9aa822b2ec7c7
Gavini Shasank <130299325+GaviniShasank@users.noreply.github.com>
2025-08-26 23:38:16 +0530

    Update Signin.jsx

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -15,7 +15,7 @@ const [email,setemail]=useState("");
 const [password,setpassword]=useState("");
 const handleLogin = async () => {
   try {
-    const { data } = await api.post("https://course-project-backend-liox.onrender.com/google-auth/login", {
+    const { data } = await api.post("https://course-project-backend-liox.onrender.com/login", {
       email,
       password,
     });
@@ -41,7 +41,7 @@ const handleLogin = async () => {
         },
       });
       const profile = await res.json();
-      const { data } = await api.post("https://course-project-backend-liox.onrender.com/google-auth/google-auth", {
+      const { data } = await api.post("https://course-project-backend-liox.onrender.com/google-auth", {
         email: profile.email,
         name: profile.name,
         picture: profile.picture,
@@ -190,4 +190,5 @@ const handleLogin = async () => {
 }
 
 export default Signin;
+
 
