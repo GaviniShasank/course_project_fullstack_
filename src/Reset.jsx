80a5332fa023549922a6c18d74e17e4527c924be
Gavini Shasank <130299325+GaviniShasank@users.noreply.github.com>
2025-08-27 17:50:42 +0530

    Update Reset.jsx

--- src/Reset.jsx
+++ src/Reset.jsx
@@ -21,7 +21,7 @@ function ResetPassword() {
 
     try {
       setLoading(true);
-      const res = await axios.post("http://localhost:3000/reset-password", {
+      const res = await axios.post("https://course-project-backend-liox.onrender.com/reset-password", {
         email,
         otp,
         newPassword,
@@ -120,3 +120,4 @@ function ResetPassword() {
 }
 
 export default ResetPassword;
+
